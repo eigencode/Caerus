920452c9f468da446fc8db2462d0459c79669caa
cepstralspike <cepstralspike@users.noreply.github.com>
2014-05-06 13:41:02 -0400

    Update ezdfstree.py
    Added vim folding '{{{}}}' comments. Changed some names to make them more descriptive. Changed logic to no longer use line counts on files. Sorted output no longer contains ctrlA characters ( cleanCurrentLine ). openNextCarton only adds to pantry for filetype that is all decimal digits.

--- ezdfstree.py
+++ ezdfstree.py
@@ -10,15 +10,68 @@ import urllib.request
 import errno
 import datetime
 # }}}
-
+# {{{ 'itemsPerCarton' is the number of lines in each merge file.
+# The number of cartons will be the total number of directory
+# elements divided by the number 'itemsPerCarton'.
+# The memory consumed by the script in each partial sort
+# increases as 'itemsPerCarton' is increased.
+# The memory consumed by the script in the final merge
+# increases as 'itemsPerCarton' is decreased, but since
+# the merge is generaly less memory intensive, memory
+# is not generally the limiting factor for a merge. OTOH 
+# if 'itemsPerCarton' were set to 1, then the merge memory-usage 
+# would essentially be the same as if 'itemsPerCarton' were 
+# greater than the total number of items to be sorted. 
+# See 'Art of Computer Programming, Volume 3: Sorting 
+# and Searching' ISBN-13: 978-0201896855
 itemsPerCarton = 8191
-topNode = os.getcwd()
-pantry = {}
-carton = []
-cartonIdx = {}
-dfsIndex = -1
-ctrlA = str( chr( 1 ) )
+# }}}
+# {{{ 'topNode'
+# start directory descend here
 #
+topNode = os.getcwd()
+# }}}
+# {{{ 'pantry' is a dictionary whose
+# keys are the names of all the 
+# merge files
+#
+pantry = {}
+# }}}
+# {{{ 'carton' is an array which contains the actual
+# directory listing data for each merge file
+#
+carton = []
+# }}}
+# {{{ 'cartonIdx' contains the fullpath names of all the
+# carton files as 'keys' and the number of entries 
+# in each carton as 'values'.
+#
+cartonIdx = {}
+# }}}
+# {{{ 'dfsIndex' is a unique base-56 encoded integer
+# associated with each unique directory element
+# that makes possible, putting directory entries 
+# back in (their original pre-sorted) order.
+#
+dfsIndex = -1
+# }}}
+# {{{ 'nullstr' syntactic sugar for ''
+#
+nullstr = ''
+# }}}
+# {{{ 'space' syntactic sugar for ' '
+#
+space = str( chr( 32 ) )
+# }}}
+# {{{ 'Tab' syntactic sugar for tab
+#
+tabChr = str( chr( 9 ) )
+# }}}
+# {{{ 'ctrlA' syntactic sugar for control-a
+#
+ctrlA = str( chr( 1 ) )
+# }}}
+# {{{ Number Base Alphabets
 # I use base 56 for Inode Numbers on NTFS
 # because the inodes can get pretty huge
 # I only track INodes so I can track hard 
@@ -29,25 +82,25 @@ B56 = "0123456789ABCDEFGHJKMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz"
 # I use 3 hex digits to number my carton 
 # files ( most people call them buckets 
 # but in my mind the bucket does not belong
-# in a pantry whereas a carton jus might)
+# in a pantry whereas a carton just might)
 #
 B16 = "0123456789ABCDEF"
 #
 # finally I use simple base 10 for nix inodes
 # but since I select these encodings via 
-# the iNodeBase dictionary, the logic is
+# the iNodeBase dictionary, the inline logic is
 # same for android and windows
 #
 B10 = "0123456789"
-
-#
+# }}}
+# {{{ 'elementTagHash' 
 # an element is either a...
 # 'F'ile
 # 'D'irectory
 # 'L'ink
 # 'U'nknown
 #
-elementTagHash = {  # {{{
+elementTagHash = {  
 		0b000:'U',
 		0b001:'F',
 		0b010:'D',
@@ -57,13 +110,12 @@ elementTagHash = {  # {{{
 		0b110:'LD',
 		0b111:'LD'
 } # }}}
-
 # {{{ regular expressions 
 
 #
 # documentation claims this is 
 # unnecessary. As of 20140325
-# python installed in fultonJSheen
+# python installed on host fultonJSheen
 # does not treat <tab> <newline> <cr>
 # as whitespace 
 #
@@ -105,12 +157,12 @@ skiplist = {
 	'///C:/%24recycle.bin':'win32'
 }
 
-dstAltDirHash = {
+alternateLogDirHash = {
  'linux-armv7l':os.getenv( 'EXTERNAL_STORAGE' ),
 		'win32':os.getenv( 'TEMP' ),
 }
 
-dstDirHash = { 
+logDirHash = { 
  'linux-armv7l': '/mnt/sdcard/00/log/tox',
 		'win32': 'C:/etc/tox'
 }
@@ -127,35 +179,55 @@ iNodeBase = {
 
 
 # }}}
-
-class InFileTupple:  # {{{
+class InputMergeObj:  # {{{
 	def __init__( self, fName ):
 		self.N = fName
-		self.lineCnt = pantry[ fName ]
+		self.lineKey = ctrlA
 		try:
+			#
+			# at object instantiation read the first 
+			# line in the text file and extract the
+			# sort key ( full path name )
+			#
 			self.H = open( fName, 'rt', encoding='utf-8' )
 			self.currentLine = self.H.readline()
-			self.lineKey = self.currentLine.split( ctrlA )[ -1 ]
+			if self.currentLine:
+				self.lineKey = self.currentLine.split( ctrlA )[ -1 ]
 		except:
 			errMsg  = "** <openInFile> == "
 			errmsg += fName
-			errmsg += " cannot read this file**"
+			errmsg += " cannot read this file **"
 			sys.exit( errMsg )
 
 	def nxtLine( self ):
-		if self.lineCnt:
+		self.lineKey = ctrlA  # default the key to assume EOF
+		if self.currentLine:
+			#
+			# the current line is not empty
+			# so the end of file has not been 
+			# reached
+			#
 			self.currentLine = self.H.readline()
-			self.lineKey = self.currentLine.split( ctrlA )[ -1 ]
-			self.lineCnt -= 1
-			if 0 == self.lineCnt:
+			if self.currentLine:
+				self.lineKey = self.currentLine.split( ctrlA )[ -1 ]
+			else:
 				self.H.close()
-		else:
-			self.currentLine = ""
-			self.lineKey = ctrlA
+
 		return self.lineKey
 		 
-#}}}
+	def cleanCurrentLine( self ):
+		#
+		# clean line contains no ctrlA characters
+		# all fields are space seperated except the
+		# last field which is seperated with at tab 
+		# character
+		#
+		self.outData = self.currentLine.split( ctrlA )
+		self.fullPath = self.outData.pop()
+		self.metaData = space.join( self.outData )
+		return tabChr.join( [ self.metaData, self.fullPath ] )
 
+#}}}
 class FsysElement: # {{{
 	def __init__(self):
 		self.Size = 0
@@ -163,22 +235,19 @@ class FsysElement: # {{{
 		self.TagKey = 0
 		self.Inode = 0
 		self.Tag = ' U'
-		self.LinkPtr = ""
-		self.dfsIndex = str( int_encode( dfsIndex ) ).zfill( 4 )
+		self.LinkPtr = nullstr
+		self.dfsIndex = str( int_encode( dfsIndex, B56) ).zfill( 4 )
 # }}}
-
 def microSecTS(): # {{{
-	return datetime.datetime.now().strftime( 'T%Y%m%d.%H%M%S.%f' )
+	return datetime.datetime.now().strftime( 'T%Y%m%d.%H%M%S.%f' + space )
 # }}}
-
 def createStamp(): # {{{
 	return time.strftime( '.%Y%m%d.%H%M%S.', time.localtime() )
 # }}}
-
 def mkdir_p( path ): # {{{
 	#
 	# I clipped this from somewhere.
-	# is seems to work. But I dont
+	# it seems to work. But I dont
 	# know for sure what 'pass' does here
 	#
     try:
@@ -188,7 +257,6 @@ def mkdir_p( path ): # {{{
             pass
         else: raise
 # }}}
-
 def int_encode( num, alphabet=B56 ): # {{{
     #
 	# Encode a number in Base X
@@ -205,85 +273,80 @@ def int_encode( num, alphabet=B56 ): # {{{
         num = num // base
         arr.append( alphabet[ rem ] )
     arr.reverse()
-    return ''.join( arr )
+    return nullstr.join( arr )
 # }}}
-
-def getDstDirName(): 
+def establishLogDir(): # {{{
 	# {{{ dst Directory Logic
-	if not sys.platform in dstDirHash:
+	if not sys.platform in logDirHash:
 		errMsg = "[BAILINGOUT]::** sys.platform == " + sys.platform + " is not supported **"
 		sys.exit( errMsg )
 
-	dstDir = dstDirHash[ sys.platform ]
+	directoryPath = logDirHash[ sys.platform ]
 
-	if os.path.exists( dstDir ):
-		dstAlternate = dstDir
+	if os.path.exists( directoryPath ):
+		alternatePath = directoryPath
 		altCount = 0
-		while os.path.isfile( dstAlternate ):
+		while os.path.isfile( alternatePath ):
 			#
-			# prefered dstDir exists as a file
+			# prefered directoryPath exists as a file
 			#
-			dstAlternate = dstDir + "." + str( altCount )
+			alternatePath = directoryPath + "." + str( altCount )
 			altCount += 1
 		if altCount:
 			#
 			# Create alternate dst directory
 			# 
-			dstDir = dstAlternate
+			directoryPath = alternatePath
 			try:
-				mkdir_p( dstDir )
+				mkdir_p( directoryPath )
 			except:
-				dstDir = dstAltDirHash[ sys.platform ]
+				directoryPath = alternateLogDirHash[ sys.platform ]
 	else:		
 		try:
-			mkdir_p( dstDir )
+			mkdir_p( directoryPath )
 		except:
-			dstDir = dstAltDirHash[ sys.platform ]
+			directoryPath = alternateLogDirHash[ sys.platform ]
 
-	if not os.path.isdir( dstDir ):
-		errMsg  = "<dstDir> == "
-		errmsg += dstDir
+	if not os.path.isdir( directoryPath ):
+		errMsg  = "<directoryPath> == "
+		errmsg += directoryPath
 		errmsg += " must be a directory"
 		sys.exit( errMsg )
 	else:
-		if not os.access( dstDir, os.W_OK ):
-			errMsg  = "<dstDir> == "
-			errmsg += dstDir
+		if not os.access( directoryPath, os.W_OK ):
+			errMsg  = "<directoryPath> == "
+			errmsg += directoryPath
 			errmsg += " must be a writable directory"
 			sys.exit( errMsg )
 
 	# }}}
-	return dstDir
+	return directoryPath
 # }}}
-
 def openOutFile( fN ): # {{{
 	try:
 		handle = open( fN, 'wt', encoding='utf-8' )
 	except:
 		errMsg  = "** <openOutFile> == "
 		errmsg += fN
-		errmsg += " cannot write to this file**"
+		errmsg += " cannot write to this file **"
 		sys.exit( errMsg )
 
 	return handle
 
 # }}}
-
 def openInFile( fN ): # {{{
 	try:
 		handle = open( fN, 'rt', encoding='utf-8' )
 	except:
 		errMsg  = "** <openInFile> == "
 		errmsg += fN
-		errmsg += " cannot read this file**"
+		errmsg += " cannot read this file **"
 		sys.exit( errMsg )
 
 	return handle
 
 # }}}
-
-
-def openNextCarton( nameType, stamp ): # {{{
+def openNxtOutFile( nameType, stamp ): # {{{
 	baseName = topNode
 	baseName = leadingDrive.sub( "\\1.slash.", baseName )
 	baseName = leadingSlash.sub( "slash.", baseName )
@@ -294,9 +357,11 @@ def openNextCarton( nameType, stamp ): # {{{
 	stringMatches = allDigits.match( nameType )
 	if stringMatches:
 		#
-		# a name type of all digits
-		# is a temporary carton file
-		# cartons fill a pantry.
+		# A name type of all digits
+		# is a temporary carton file.
+		#
+		# Cartons fill a pantry.
+		#
 		# All the cartons in the pantry
 		# eventually get placed into a 
 		# single crate (a srt.txt file).
@@ -305,7 +370,7 @@ def openNextCarton( nameType, stamp ): # {{{
 		nameType = str( nameType ).zfill( 3 ) 
 		suffix = ".tmp"
 
-	outFName  =	getDstDirName()
+	outFName  =	establishLogDir()
 	outFName += "/"
 	outFName += baseName
 	outFName += stamp
@@ -313,11 +378,13 @@ def openNextCarton( nameType, stamp ): # {{{
 	outFName += suffix
 	outFName  = anyPeriod.sub( ".", outFName )
 	outFName  = anySlash.sub( "/", outFName )
-	pantry[ outFName ] = -1
+
+	if ".tmp" == suffix:
+		pantry[ outFName ] = 0
+
 	outFHandle = openOutFile( outFName )
 	return { "outFHandle":outFHandle, "outFName":outFName }
 # }}}
-
 def int_decode( string, alphabet=B56 ): # {{{
 	#
 	# Decode a Base X encoded string into the number
@@ -338,7 +405,6 @@ def int_decode( string, alphabet=B56 ): # {{{
 
     return num
 # }}}
-
 def WriteFsysElementInfo( path, fH, fN ): # {{{
 	#
 	# send a line to a text file
@@ -361,36 +427,26 @@ def WriteFsysElementInfo( path, fH, fN ): # {{{
 	msg += "\n"
 	fH.write( msg )
 # }}}
-
+# {{{ Main descent Loop Initialization
 cartonNumber = 0
-timeStamp = createStamp()
+uniqIdStamp = createStamp()
 #
 # error log file is a special carton
 #
-rslt = openNextCarton( "log", timeStamp )
+rslt = openNxtOutFile( "log", uniqIdStamp )
 dstLogFName = rslt[ "outFName" ]
 fLog = rslt[ "outFHandle" ]
 #
-# dont confuse carton processing with 
-# the error file
-#
-pantry.pop( dstLogFName )
-#
 # error log file is a special carton
 #
-rslt = openNextCarton( "raw", timeStamp )
+rslt = openNxtOutFile( "raw", uniqIdStamp )
 dstRawFName = rslt[ "outFName" ]
 fRaw = rslt[ "outFHandle" ]
-#
-# dont confuse carton processing with 
-# the raw output file
-#
-pantry.pop( dstRawFName )
 
 
 dirStack = []
 dirStack.insert( 0, topNode )
-cartonLevel = -1
+# }}}
 while len( dirStack ): # {{{ Main Outer Loop
 
 	thisDir = dirStack.pop()
@@ -403,20 +459,18 @@ while len( dirStack ): # {{{ Main Outer Loop
 	try:
 		dirListing = os.listdir( thisDir )
 	except:
-		fLog.write( str( int_encode( dfsIndex ) ).zfill( 4 )	+
+		fLog.write( microSecTS()								+ 
+					str( int_encode( dfsIndex ) ).zfill( 4 )	+
 					" <<< [[[Exception 0 Triggered]]]::"		+ 
-					thisDir									+ 
+					thisDir										+ 
 					">>>\n" )
 
 		dirListing = []
 
 	for eName in dirListing: # {{{ Main inner Loop
-		cartonLevel += 1
 		dfsIndex += 1
 		fullPath = os.path.join( thisDir, eName )
-		cartonIdx [ fullPath ] = cartonLevel
 		e = FsysElement()
-
 
 		try:
 			e.Size    = os.lstat( fullPath )[ stat.ST_SIZE ]
@@ -433,127 +487,132 @@ while len( dirStack ): # {{{ Main Outer Loop
 				e.LinkPtr = ' -> ' + os.readlink( fullPath ) 
 		except:
 			# {{{ Exception Triggered
-			fLog.write( str( int_encode( dfsIndex ) ).zfill( 4 )	+
+			fLog.write( microSecTS()								+ 
+						str( int_encode( dfsIndex ) ).zfill( 4 )	+
 						" <<< [[[Exception 1 Triggered]]]::"		+ 
-						fullPath						+ 
+						fullPath									+ 
 						">>>\n"  )
 			# }}}
 
+		cartonIdx [ fullPath ] = len( carton )
 		carton.append( e )
 		WriteFsysElementInfo( fullPath, fRaw, dstRawFName )
 
 		if 'D' == e.Tag:
 			dirStack.insert( 0, fullPath )
 
-
-		if cartonLevel >= itemsPerCarton: # {{{
-			rslt = openNextCarton( str( cartonNumber ), timeStamp )
+		if itemsPerCarton == len( carton ): # {{{
+			rslt = openNxtOutFile( str( cartonNumber ), uniqIdStamp )
 			dstFName = rslt[ "outFName" ]
 			fOut = rslt[ "outFHandle" ]
-			fLog.write( microSecTS() + ' > ' + dstFName + "\n")
+			fLog.write( microSecTS() + '> ' + dstFName + "\n")
 			#
 			# pantry dictionary contains the full path names 
 			# of all the carton files as indexes.
 			# associated with each carton file is a linecount
 			#
-			pantry[ dstFName ] = cartonLevel
+			pantry[ dstFName ] = len( carton )
 			for fullPath in sorted( cartonIdx.keys() ): # {{{
 				WriteFsysElementInfo( fullPath, fOut, dstFName )
 			# }}}
 			fOut.close()
 			#
-			# I only keep the active carton in memory
-			# clear out the old. Make room for the new
-			# this python slice semantics will take me
-			# some getting-used-to
+			# I only keep the 'active' carton in memory.
+			# So, clear out the old. Make room for the new.
+			# This python slice semantics will take me
+			# some getting-used-to.
 			#
 			carton[:] = []
 			#
 			# Carton has been cleared so must also
-			# be the index
+			# be the carton index.
 			#
 			cartonIdx.clear()
-			cartonLevel = -1
 			cartonNumber += 1
 		# }}}
 		# }}}
 # }}}
-
-if cartonLevel >= 0:
+# {{{ Main descent Loop Cleanup
+if len( carton ): # {{{
 	#
 	# usually a partially filled carton 
-	# will be left over. This will cover that
+	# will be left over. So, manage that condition.
 	#
-	rslt = openNextCarton( str( cartonNumber ), timeStamp )
+	rslt = openNxtOutFile( str( cartonNumber ), uniqIdStamp )
 	dstFName = rslt[ "outFName" ]
 	fOut = rslt[ "outFHandle" ]
-	fLog.write( microSecTS() + ' > ' + dstFName  + "\n")
-	pantry[ dstFName ] = cartonLevel
+	fLog.write( microSecTS() + '> ' + dstFName  + "\n")
+	pantry[ dstFName ] = len( carton )
 	for fullPath in sorted( cartonIdx.keys() ): # {{{
 		WriteFsysElementInfo( fullPath, fOut, dstFName )
 	# }}}
-	fOut.close()
-#
+	fOut.close() # }}}
 # recursive descent is complete
 # now I need to merge all of my
 # cartons into a single crate
+# which will be sorted by fullpathname
 #
 fRaw.close()
-
-kList = list( pantry.keys() )
-for fName in kList:
-	if 0 > pantry[ fName ]:
-		pantry.pop( fName )
-		
-kList[:] = []
+# }}}
+# {{{ Initialize the merge operation
+#
+# put the names of all
+# merge files in the mergeQ
+#
 mergeQ = []
-kList = list( pantry.keys() )
-for fName in kList:
-	bucket = InFileTupple( fName )
+tmpFileList = list( pantry.keys() )
+for fName in tmpFileList:
+	#
+	# open temp file for reading
+	#
+	bucket = InputMergeObj( fName )
+	#
+	# put the handle, FileName pair in the queue
+	#
 	mergeQ.append( bucket )
 
-rslt = openNextCarton( "srt", timeStamp )
+rslt = openNxtOutFile( "srt", uniqIdStamp )
 dstSrtFName = rslt[ "outFName" ]
 fSrt = rslt[ "outFHandle" ]
-#
-# dont confuse carton processing with 
-# the sorted output file
-#
-pantry.pop( dstSrtFName )
 
 therezWork2do = True
-while therezWork2do:
+# }}}
+while therezWork2do: # {{{ Main Merge Loop
 	minIdx = 0
 	if 1 < len( mergeQ ):
 		for idx in list( range( 1, len( mergeQ ) ) ):
 			if mergeQ[ idx ].lineKey < mergeQ[ minIdx ].lineKey:
 				minIdx = idx
 		bucket = mergeQ[ minIdx ]
-		fSrt.write( bucket.currentLine )
+		fSrt.write( bucket.cleanCurrentLine() )
 		if ctrlA == bucket.nxtLine():
-			fLog.write( microSecTS() + ' < ' + mergeQ[ minIdx ].N + "\n")
+			fLog.write( microSecTS() + '< ' + mergeQ[ minIdx ].N + "\n" )
 			mergeQ.pop( minIdx )
 			minIdx = 0
 	else:
 		therezWork2do = False
-
+# }}}
+# {{{ Merge Cleanup
 bucket = mergeQ[ 0 ]
-fSrt.write( bucket.currentLine )
-while ctrlA != bucket.nxtLine():
-	fSrt.write( bucket.currentLine )
-
-fLog.write( microSecTS() + ' < ' + mergeQ[ 0 ].N  + "\n")
+fSrt.write( bucket.cleanCurrentLine() )
+while ctrlA != bucket.nxtLine(): # {{{
+	#
+	# write out all the lines that remain
+	# is the last bucket
+	#
+	fSrt.write(  bucket.cleanCurrentLine() )
+# }}}
+fLog.write( microSecTS() + '< ' + mergeQ[ 0 ].N  + "\n")
 mergeQ.pop( 0 )
-
 fSrt.close()
 fLog.close()
 #
 # cleanup the temp files
 #
-kList = list( pantry.keys() )
-for fName in kList:
+tmpFileList = list( pantry.keys() )
+for fName in tmpFileList:
 	os.remove( fName )
-
+ }}}
 print( dstRawFName )
 print( dstSrtFName )
 print( dstLogFName )
