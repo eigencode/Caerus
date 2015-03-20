f6f9a7ebca1a020558f4f3e02fa67d980a56ac57
cepstralspike <cepstralspike@users.noreply.github.com>
2015-03-20 19:37:54 -0400

    Update ezdfstree.py
    # Added changes necessary to make a full device manifest run to completion
    # on a Samsung SM-P607T lt03ltetmo with python-3.4.2

--- ezdfstree.py
+++ ezdfstree.py
@@ -1,4 +1,13 @@
 #!/system/xbin/env python3
+# {{{
+# Added changes necessary to make a full device manifest run to completion
+# on a Samsung SM-P607T lt03ltetmo with python-3.4.2
+# }}}
+# {{{
+# Recursively descend the current directory. Use minimum memory resources. 
+# Do a merge sort on the resulting output using the full path name as the 
+# sort key.
+# }}}
 # {{{ imports
 import sys
 import argparse
@@ -13,64 +22,69 @@ import tempfile
 import shutil
 import socket
 # }}}
-# {{{ 'itemsPerCarton' is the number of lines in each merge file.
+# {{{ `itemsPerCarton` is the number of lines in each merge file.
 # The number of cartons will be the total number of directory
-# elements divided by the number 'itemsPerCarton'.
+# elements divided by the number `itemsPerCarton`.
 # The memory consumed by the script in each partial sort
-# increases as 'itemsPerCarton' is increased.
+# increases as `itemsPerCarton` is increased.
 # The memory consumed by the script in the final merge
-# increases as 'itemsPerCarton' is decreased, but since
+# increases as `itemsPerCarton` is decreased, but since
 # the merge is generaly less memory intensive, memory
 # is not generally the limiting factor for a merge. OTOH 
-# if 'itemsPerCarton' were set to 1, then the merge memory-usage 
-# would essentially be the same as if 'itemsPerCarton' were 
+# if `itemsPerCarton` were set to 1, then the merge memory-usage 
+# would essentially be the same as if `itemsPerCarton` were 
 # greater than the total number of items to be sorted. 
-# See 'Art of Computer Programming, Volume 3: Sorting 
-# and Searching' ISBN-13: 978-0201896855
+# See `Art of Computer Programming, Volume 3: Sorting 
+# and Searching` ISBN-13: 978-0201896855
 itemsPerCarton = 8191
 # }}}
-# {{{ 'topNode'
+# {{{ `topNode`
 # start directory descend here
 #
 topNode = os.getcwd()
 # }}}
-# {{{ 'pantry' is a dictionary whose
+# {{{ `fsEncoding`
+# default file system encoding for this system
+#
+fsEncoding = 'utf-8' # sys.getfilesystemencoding()
+# }}}
+# {{{ `pantry` is a dictionary whose
 # keys are the names of all the 
 # merge files
 #
 pantry = {}
 # }}}
-# {{{ 'carton' is an array which contains the actual
+# {{{ `carton` is an array which contains the actual
 # directory listing data for each merge file
 #
 carton = []
 # }}}
-# {{{ 'cartonIdx' contains the fullpath names of all the
-# carton files as 'keys' and the number of entries 
-# in each carton as 'values'.
+# {{{ `cartonIdx` contains the fullpath names of all the
+# carton files as `keys` and the number of entries 
+# in each carton as `values`.
 #
 cartonIdx = {}
 # }}}
-# {{{ 'dfsIndex' is a unique base-56 encoded integer
+# {{{ `dfsIndex` is a unique base-56 encoded integer
 # associated with each unique directory element
 # that makes possible, putting directory entries 
 # back in (their original pre-sorted) order.
 #
 dfsIndex = -1
 # }}}
-# {{{ 'nullstr' syntactic sugar for ''
+# {{{ `nullstr` syntactic sugar for ``
 #
 nullstr = ''
 # }}}
-# {{{ 'space' syntactic sugar for ' '
+# {{{ `space` syntactic sugar for ` `
 #
 space = str( chr( 32 ) )
 # }}}
-# {{{ 'Tab' syntactic sugar for tab
+# {{{ `Tab` syntactic sugar for tab
 #
 tabChr = str( chr( 9 ) )
 # }}}
-# {{{ 'ctrlA' syntactic sugar for control-a
+# {{{ `ctrlA` syntactic sugar for control-a
 #
 ctrlA = str( chr( 1 ) )
 # }}}
@@ -96,12 +110,12 @@ B16 = "0123456789ABCDEF"
 #
 B10 = "0123456789"
 # }}}
-# {{{ 'elementTagHash' 
+# {{{ `elementTagHash` 
 # an element is either a...
-# 'F'ile
-# 'D'irectory
-# 'L'ink
-# 'U'nknown
+# `F`ile
+# `D`irectory
+# `L`ink
+# `U`nknown
 #
 elementTagHash = {  
 		0b000:'U',
@@ -156,31 +170,39 @@ allDigits = re.compile(  r"""
 	""".strip( WS ), re.X | re.I )
 
 skiplist = { 
-	'/proc':'linux-armv7l',
-	'///C:/%24recycle.bin':'win32'
+                   '/proc': [ 'linux-armv7l', 'linux' ],
+            '/sys/devices': [ 'linux-armv7l', 'linux' ],
+                    '/dev': [ 'linux-armv7l', 'linux' ],
+                '/sys/dev': [ 'linux-armv7l', 'linux' ],
+	'///C:/%24recycle.bin': [ 'win32' ]
 }
 
 alternateLogDirHash = {
+        'linux':os.getenv( 'EXTERNAL_STORAGE' ),
  'linux-armv7l':os.getenv( 'EXTERNAL_STORAGE' ),
 		'win32':os.getenv( 'TEMP' ),
 }
 
 logDirHash = { 
+        'linux': '/storage/extSdCard/00/log/tox',
  'linux-armv7l': '/mnt/sdcard/00/log/tox',
 		'win32': 'C:/etc/tox'
 }
 
 drpBxDirHash = { 
+        'linux': '/storage/emulated/0/00/log/tox',
  'linux-armv7l': '/mnt/sdcard/00',
 		'win32': 'C:/drpbx/Dropbox/tox'
 }
 
 iNodeFldWdth = { 
+        'linux': 10,
  'linux-armv7l': 10,
 		'win32': 12
 }
 
 iNodeBase = { 
+        'linux': B10,
  'linux-armv7l': B10,
 		'win32': B56
 }
@@ -197,7 +219,7 @@ class InputMergeObj:  # {{{
 			# line in the text file and extract the
 			# sort key ( full path name )
 			#
-			self.H = open( fName, 'rt', encoding='utf-8' )
+			self.H = open( fName, 'rt', encoding=fsEncoding )
 			self.currentLine = self.H.readline()
 			if self.currentLine:
 				self.lineKey = self.currentLine.split( ctrlA )[ -1 ]
@@ -256,7 +278,7 @@ def mkdir_p( path ): # {{{
 	#
 	# I clipped this from somewhere.
 	# it seems to work. But I dont
-	# know for sure what 'pass' does here
+	# know for sure what `pass` does here
 	#
     try:
         os.makedirs( path )
@@ -332,7 +354,7 @@ def establishDestinationDir( dirHash ): # {{{
 # }}}
 def openOutFile( fN ): # {{{
 	try:
-		handle = open( fN, 'wt', encoding='utf-8' )
+		handle = open( fN, 'wt', encoding=fsEncoding )
 	except:
 		errMsg  = "** <openOutFile> == "
 		errmsg += fN
@@ -344,7 +366,7 @@ def openOutFile( fN ): # {{{
 # }}}
 def openInFile( fN ): # {{{
 	try:
-		handle = open( fN, 'rt', encoding='utf-8' )
+		handle = open( fN, 'rt', encoding=fsEncoding )
 	except:
 		errMsg  = "** <openInFile> == "
 		errmsg += fN
@@ -403,6 +425,27 @@ def nextOutFile( nameType, stamp ): # {{{
 
 	return { "outFHandle":outFHandle, "outFName":outFName, "baseName":baseName }
 # }}}
+def coerse2str( dataIn ): # {{{
+	#
+	# As of python version 3.1: On some systems, conversion using the file system encoding may fail. 
+	# In this case, Python uses the surrogateescape encoding error handler, which means that undecodable 
+    # bytes are replaced by a Unicode character U+DCxx on decoding.
+	# This program only generates a manifest of files. So the filname encoding in the 
+	# manifest need not be the same as the file name encoding on the physical medium
+	#
+	if isinstance( dataIn, bytes ):
+		retVal = dataIn.decode( fsEncoding, errors='surrogateescape' ) 
+	elif isinstance( dataIn, str ):
+		dataInBytes = dataIn.encode( fsEncoding, errors='surrogateescape') 
+		retVal = dataInBytes.decode( fsEncoding, errors='surrogateescape'  )
+	else:
+		errMsg  = "** <coerse2str> dataIn == "
+		errmsg += dataIn
+		errmsg += " 'dataIn' must be bytes or str **"
+		sys.exit( errMsg )
+
+	return retVal # Instance of str
+# }}}
 def int_decode( string, alphabet=B56 ): # {{{
 	#
 	# Decode a Base X encoded string into the number
@@ -443,7 +486,8 @@ def WriteFsysElementInfo( path, fH, fN ): # {{{
 	msg += path
 	msg += e.LinkPtr
 	msg += "\n"
-	fH.write( msg )
+	msg2write = coerse2str( msg )
+	fH.write( msg2write )
 # }}}
 # {{{ Main descent Loop Initialization
 cartonNumber = 0
@@ -470,16 +514,16 @@ while len( dirStack ): # {{{ Main Outer Loop
 
 	thisDir = dirStack.pop()
 
-	urlPath = urllib.request.pathname2url( thisDir ) 
+	urlPath = urllib.request.pathname2url( coerse2str( thisDir ) )
 	if urlPath in skiplist : 
-		if skiplist[ urlPath ] == sys.platform :
+		osList = skiplist[ urlPath ]
+		if sys.platform in osList :
 			continue
-
 	try:
 		dirListing = os.listdir( thisDir )
 	except:
 		fLog.write( microSecTS()								+ 
-					str( int_encode( dfsIndex ) ).zfill( 4 )	+
+					str( int_encode( dfsIndex, B56 ) ).zfill( 4 )	+
 					" <<< [[[Exception 0 Triggered]]]::"		+ 
 					thisDir										+ 
 					">>>\n" )
@@ -507,7 +551,7 @@ while len( dirStack ): # {{{ Main Outer Loop
 		except:
 			# {{{ Exception Triggered
 			fLog.write( microSecTS()								+ 
-						str( int_encode( dfsIndex ) ).zfill( 4 )	+
+						str( int_encode( dfsIndex, B56 ) ).zfill( 4 )	+
 						" <<< [[[Exception 1 Triggered]]]::"		+ 
 						fullPath									+ 
 						">>>\n"  )
@@ -536,7 +580,7 @@ while len( dirStack ): # {{{ Main Outer Loop
 			# }}}
 			fOut.close()
 			#
-			# I only keep the 'active' carton in memory.
+			# I only keep the `active` carton in memory.
 			# So, clear out the old. Make room for the new.
 			# This python slice semantics will take me
 			# some getting-used-to.
